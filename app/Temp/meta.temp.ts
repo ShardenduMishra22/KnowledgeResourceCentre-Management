9d9b1fb4bb80119dec8e741a7b531a8bb3a2f4a1
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-01-26 18:27:59 +0530

    Add metadata file, update page structure, and include new SVG icons

---
+++ app/Temp/meta.temp.ts
@@ -0,0 +1,5 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'This is sample File',
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default Page;
